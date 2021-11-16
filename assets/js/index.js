6a969ae0c7128bfd65753b37dcc908fa4042a199
TheLuisAbarca <arclaav.adm@gmail.com>
2021-11-16 14:38:53 -0500

    Add logo and favicon to the page.

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,3 +20,44 @@ listEl.forEach((el) => {
     bodyel.style.overflow = 'auto';
   });
 });
+
+const content01 = {
+    name: 'Joselyn Mero',
+    position: 'CEO',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    speaker_image: 'src/img/img_Placeholder.png',
+};
+const content02 = {
+    name: 'Joselyn Mero',
+    position: 'CEO',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    speaker_image: 'src/img/img_Placeholder.png',
+};
+const content03 = {
+    name: 'Joselyn Mero',
+    position: 'CEO',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    speaker_image: 'src/img/img_Placeholder.png',
+};
+const content04 = {
+    name: 'Joselyn Mero',
+    position: 'CEO',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    speaker_image: 'src/img/img_Placeholder.png',
+};
+const content05 = {
+    name: 'Joselyn Mero',
+    position: 'CEO',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    speaker_image: 'src/img/img_Placeholder.png',
+};
+const content06 = {
+    name: 'Joselyn Mero',
+    position: 'CEO',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    speaker_image: 'src/img/img_Placeholder.png',
+};
+const allcontent = [content01, content02, content03, content04];
+
+function htmlWorksTemplate(index) {
+}
