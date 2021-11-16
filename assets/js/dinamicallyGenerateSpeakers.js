5b6d2e4dc2443a20be7d2fb75b9db06e59b5ec25
TheLuisAbarca <arclaav.adm@gmail.com>
2021-11-16 17:00:01 -0500

    Separate functions in two files and loaded in the respective page when its needed.

---
+++ assets/js/dinamicallyGenerateSpeakers.js
@@ -0,0 +1,67 @@
+const content01 = {
+    name: 'Joselyn Mero',
+    position: 'CEO',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    speaker_image: 'assets/img/profile.png',
+};
+const content02 = {
+    name: 'Joselyn Mero',
+    position: 'CEO',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    speaker_image: 'assets/img/profile.png',
+};
+const content03 = {
+    name: 'Joselyn Mero',
+    position: 'CEO',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    speaker_image: 'assets/img/profile.png',
+};
+const content04 = {
+    name: 'Joselyn Mero',
+    position: 'CEO',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    speaker_image: 'assets/img/profile.png',
+};
+const content05 = {
+    name: 'Joselyn Mero',
+    position: 'CEO',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    speaker_image: 'assets/img/profile.png',
+};
+const content06 = {
+    name: 'Joselyn Mero',
+    position: 'CEO',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    speaker_image: 'assets/img/profile.png',
+};
+const allcontent = [content01, content02, content03, content04, content05, content06];
+
+function htmlSpeakersTemplate(index) {
+    return `
+    <div class="row">
+      <div class="col-4">
+        <img src="${index.speaker_image}" alt="profile" class="img-fluid">
+      </div>
+      <div class="col-8">
+        <p class="text-justify text-lato text-dark mb-0" style="font-size: 20px;">
+        ${index.name}
+        </p>
+        <p class="text-justify text-lato text-magenta" style="font-size: 15px;">
+        ${index.position}
+        </p>
+        <p class="mt-4 text-justify text-lato text-dark" style="font-size: 10px;">
+        ${index.description}
+        </p>
+      </div>
+    </div>`;
+}
+
+allcontent.forEach((element, index) => {
+    const currIndex = Number(index + 1);
+    const dinContent = document.createDocumentFragment();
+    const newDiv = document.createElement('div');
+    newDiv.className = 'col-12 col-md-6 p-3';
+    newDiv.innerHTML = htmlSpeakersTemplate(element);
+    dinContent.appendChild(newDiv);
+    document.getElementById('speakers-container').appendChild(dinContent);
+});
